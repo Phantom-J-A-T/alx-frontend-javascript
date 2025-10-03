382623c42cf8fe02136aa01677316d6b8ed4cffd
Phantom <ramonadedotun550@gmail.com>
2025-10-04 00:26:36 +0100

    Task 8

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,24 @@
+// task_3/js/crud.js
+
+export function insertRow(row) {
+    console.log('Insert row', row);
+    return Math.floor(Math.random() * Math.floor(1000));
+  }
+  
+  export function deleteRow(rowId) {
+    console.log('Delete row id', rowId);
+    return;
+  }
+  
+  export function updateRow(rowId, row) {
+    console.log(`Update row ${rowId}`, row);
+    return rowId;
+  }
+
+// task_3/js/crud.d.ts
+
+import { RowID, RowElement } from "./interface";
+
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,11 @@
+// task_3/js/interface.ts
+
+// Type RowID
+export type RowID = number;
+
+// Interface RowElement
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
